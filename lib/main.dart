c8e3e564b9fc9225a5d6fa2e0d89661a852aa34d
dangvanhuy <dangvanhuy1432002@gmail.com>
2022-06-27 13:51:26 +0700

    appnt

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:appnt/Login/login.dart';
+import 'package:appnt/Trasfer/Financial%20Report_Budget.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
